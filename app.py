8d39dc5c4268f49838f5a45d36a92c72ab728c35
DEO PRAKASH <139586510+deoprakash@users.noreply.github.com>
2025-05-31 14:20:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -971,5 +971,5 @@ def common_disease_form():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get('PORT') or 5000)
     app.run(port=port, host="0.0.0.0", debug=True)
